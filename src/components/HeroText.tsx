ffc7007f0b17d5bde1ef4e1ef16751e5a5e6a4dd
Elisha <your-email@example.com>
2026-04-25 07:59:07 +0600

    fix: fixing size for mobile text

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -22,7 +22,7 @@ export const HeroText = () => {
     <div>
       <h1
         className=" absolute  max-md:max-w-100 w-max max-w-[90vw] sm:max-w-248
-        mt-16 z-5 text-[clamp(2rem,8vw,4rem)] ml-[clamp(1.5rem,18vw,12rem)]    flex
+        mt-16 z-5 text-[clamp(1.5rem,8vw,4rem)] ml-[clamp(1.5rem,18vw,12rem)]    flex
         flex-col items-start gap-y-[clamp(0.5rem,3vw,3rem)] leading-none  max-sm:ml-0 max-sm:pl-12 max-md:pt-12 max-sm:pt-28"
       >
         <span className="block">
